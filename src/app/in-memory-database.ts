de19fafc12bcf04044a192941064cd65e0a3d5da
fillipeoa <fillipeoa1234@gmail.com>
2020-04-15 15:12:42 -0300

    adding in-memory-database for categories

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,11 @@
+import { InMemoryDbService } from "angular-in-memory-web-api";
+
+export class InMemoryDatabase implements InMemoryDbService{
+    createDb(){
+        const categories = [
+            { id:1, name:"Lazer", description:"" },
+
+        ];
+        return categories;
+    }
+}
